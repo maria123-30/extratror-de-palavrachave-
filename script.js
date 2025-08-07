f301b9816f34cb066466672e9c1b68bb0d3685f8
maria123-30 <m.rosa30@escola.pr.gov.br>
2025-08-07 08:41:53 -0300

    htyejytj

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
 const botaoMostraPalavras = document.querySelector('#botao-palavrachave');
 
-botaoMostraPalavras.addEventListener('click',mostraPalavrasChaves );
+botaoMostraPalavras.addEventListener('click', mostraPalavrasChaves );
 
 function mostraPalavrasChaves (){
-    const texto = document .querySelectorAll('#entrada-de-texto').value;
+    const texto = document .querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
     const palavras = texto.split(" ");
 
-    campoResultado.textContent = palavras;
+    campoResultado.textContent = palavras; 
 }
