c55668eebe77d61d738dfec05d282dbd7debbfb2
maria123-30 <m.rosa30@escola.pr.gov.br>
2025-08-07 08:35:52 -0300

    few

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ botaoMostraPalavras.addEventListener('click',mostraPalavrasChaves );
 function mostraPalavrasChaves (){
     const texto = document .querySelectorAll('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
+    const palavras = texto.split(" ");
 
-    campoResultado.textContent = texto;
+    campoResultado.textContent = palavras;
 }
