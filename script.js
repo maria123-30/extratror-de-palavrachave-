e29104a71e018b00cd3e2633b789f70392f0f094
maria123-30 <m.rosa30@escola.pr.gov.br>
2025-08-14 08:37:03 -0300

    klyilyil

--- script.js
+++ script.js
@@ -5,12 +5,12 @@ botaoMostraPalavras.addEventListener('click', mostraPalavrasChave);
 function mostraPalavrasChave() {
     const texto = document.querySelector('#entrada-de-texto').value;
     const campoResultado = document.querySelector('#resultado-palavrachave');
-    const PalavrasChave = processaTexto(texto);
+    const palavrasChave = processaTexto(texto);
 
-    campoResultado.textContent = PalavrasChave.join(", ");
+    campoResultado.textContent = palavrasChave.join(", ");
 }
-function processaTexto(texto) {
-  let palavras = texto.split(" ");
 
-  return palavras;
+function processaTexto(texto) {
+    let palavras = texto.split(/\P{L}+/u);
+    return palavras;
 }
