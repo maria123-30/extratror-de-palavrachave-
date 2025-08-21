23167a892bfe865fbd030db8abcbf6aa67753606
maria123-30 <m.rosa30@escola.pr.gov.br>
2025-08-21 08:01:54 -0300

    mnjnkj

--- script.js
+++ script.js
@@ -13,14 +13,15 @@ function mostraPalavrasChave() {
 function processaTexto(texto) {
   let palavras = texto.split(/\P{L}+/u);
 
-  let frequencias = [];
+  let frequencias = {};
 
-  for (let i in palavras){
+  for (let i of palavras) {
     frequencias[i] = 0;
-    for (let j in palavras){
-        if (palavras[i]==palavras[j]){
-            frequencias[i]++; 
-        }
+
+    for (let j of palavras) {
+      if (i == j) {
+        frequencias[i]++;
+      }
     }
   }
 
